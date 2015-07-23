f274266c2f87d7931a925095fc1c93970bf49396
sephirot47 <victorantondominguez@gmail.com>
2015-07-23 12:39:09 +0200

    Basic drag and drop

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -3,106 +3,86 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
-	public int maxTouch = 2;
-	[Range(0,31)]
-	public int layerMask = 0;
-	public float distance = 0.2f;
-	public float dampingRatio = 1;
-	public float frequency = 1.8f;
-	public float linearDrag = 1.0f;
-	public float angularDrag = 5.0f;
-	public bool centerOfMass = false;
-	private SpringJoint2D[] springJoints;
-	
-	void Start ()
-	{
-		springJoints = new SpringJoint2D[maxTouch+1];
-		
-		for (int i = 0; i < maxTouch; i++) {
-			GameObject go = new GameObject ("Dragger" + (i + 1));
-			go.transform.parent = this.transform;
-			
-			Rigidbody2D body = go.AddComponent <Rigidbody2D>() as Rigidbody2D;
-			springJoints [i] = go.AddComponent <SpringJoint2D>() as SpringJoint2D;
-			body.isKinematic = true;
-		}
-	}
+    public static readonly float Gravity = 0.2f;
+    public bool upwardsGravity = false;
+    public bool holding = false;
 
-	void onTouch ()
-	{
-		if (transform.position.y >= Screen.height / 2) {
-			Vector3 newGravity = new Vector3 (0, Mathf.Abs (Physics.gravity.y), 0);
-			Physics2D.gravity = newGravity;
-		} else {
-			Vector3 newGravity = new Vector3 (0, -Mathf.Abs (Physics.gravity.y), 0);
-			Physics2D.gravity = newGravity;
-		}
-	}
-	
-	void Update ()
-	{
-		foreach (Touch touch in Input.touches) {
-			int Id = touch.fingerId;
-			
-			if (Id < maxTouch && touch.phase == TouchPhase.Began) {
-				Camera mainCamera = FindCamera ();
-				Ray ray = mainCamera.ScreenPointToRay (touch.position);
-				RaycastHit2D hit = Physics2D.GetRayIntersection (ray, Mathf.Infinity, 1 << layerMask);
-				
-				if (hit.rigidbody != null && hit.rigidbody.isKinematic == false) {
-					springJoints [Id].transform.position = hit.point;
-					springJoints [Id].connectedBody = hit.rigidbody;
-					onTouch();
-					if (centerOfMass)
-						springJoints [Id].connectedAnchor = hit.rigidbody.centerOfMass;
-					else
-						springJoints [Id].connectedAnchor = hit.transform.InverseTransformPoint (hit.point);
-					
-					float length = (hit.transform.position - mainCamera.transform.position).magnitude;
-					StartCoroutine (DragObject (Id, length));
-				}
-			}
-		}
-	}
-	
-	IEnumerator DragObject (int Id, float length)
-	{
-		float oldDrag = springJoints [Id].connectedBody.drag;
-		float oldAngularDrag = springJoints [Id].connectedBody.angularDrag;
-		springJoints [Id].distance = distance;
-		springJoints [Id].dampingRatio = dampingRatio;
-		springJoints [Id].frequency = frequency;
-		springJoints [Id].connectedBody.drag = linearDrag;
-		springJoints [Id].connectedBody.angularDrag = angularDrag;
-		Camera mainCamera = FindCamera ();
+    public float speedX = 0.0f, speedY = 0.0f;
+    public float accX = 1.0f;
 
-			while (true) {
-			bool touchExists = false;
-			foreach (Touch touch in Input.touches) {
-				if (touch.fingerId == Id) {
-					touchExists = true;
-					Ray ray = mainCamera.ScreenPointToRay (touch.position);
-					springJoints [Id].transform.position = ray.GetPoint (length);
-				}
-			}
-			if (touchExists)
-				yield return null;
-			else
-				break;
-		}
-		
-		if (springJoints [Id].connectedBody) {
-			springJoints [Id].connectedBody.drag = oldDrag;
-			springJoints [Id].connectedBody.angularDrag = oldAngularDrag;
-			springJoints [Id].connectedBody = null;
-		}  
-	}
-	
-	Camera FindCamera ()
-	{
-		if (GetComponent<Camera>())
-			return GetComponent<Camera>();
-		else
-			return Camera.main;
-	}
+    void Start() { }
+
+    void FixedUpdate()
+    {
+        if (!holding) speedY += Gravity * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        if(holding)
+        {
+            Vector3 newPos = Vector3.Lerp(transform.position, GetWorldPoint(Input.mousePosition), Time.deltaTime * accX);
+            transform.position = new Vector3(newPos.x, newPos.y, 0.0f);
+        }
+
+        transform.position += Vector3.up * speedY * (upwardsGravity ? 1.0f : -1.0f);
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 worldTouch = GetWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast ( new Vector2( worldTouch.x, worldTouch.y ), Vector2.zero, Mathf.Infinity);
+            if (hit && hit.collider && hit.collider.gameObject == gameObject)
+            {
+                OnHold();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (holding) OnDrop();
+        }
+
+        foreach(Touch t in Input.touches)
+        {
+            Vector3 worldTouch = GetWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(worldTouch.x, worldTouch.y), Vector2.zero, Mathf.Infinity);
+            if (hit && hit.collider && hit.collider.gameObject == gameObject)
+            {
+                switch(t.phase)
+                {
+                    case TouchPhase.Canceled:
+                    case TouchPhase.Ended:
+                        hit.collider.SendMessage("OnDrop", null, SendMessageOptions.DontRequireReceiver);
+                        break;
+
+                    default:
+                        OnHold();
+                        break;
+                }
+            }
+        }
+    }
+
+    Vector3 GetWorldPoint(Vector3 screenPos)
+    {
+        return Camera.main.ScreenToWorldPoint(screenPos);
+    }
+
+    bool IsScreenUp()
+    {
+        return transform.position.y >= Camera.main.ScreenToWorldPoint( new Vector3(0.0f, Screen.height / 2, 0.0f) ).y;
+    }
+
+    void OnHold()
+    {
+        holding = true;
+        speedY = 0.0f;
+    }
+
+    void OnDrop()
+    {
+        holding = false;
+        if (IsScreenUp()) upwardsGravity = false;
+        else upwardsGravity = true;
+    }
 }
